d3a12ab8f37f59fbe60e64099c09cbfd8e7e1578
Sergioess <sergioess@hotmail.com>
2021-12-01 19:17:39 -0500

    agrega notificacion

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -7,7 +7,7 @@ const pool = require('../database/conn');
 exports.readByIdUsr = async function (req, res) {
     const id = parseInt(req.params.id)
     try {
-        const respuesta = await pool.query('select n.id as id, n.type_notification , n.created_at, n.description , n.id_objeto , u.id as usr_r_id, u.username as usr_r_name,u2.id as usr_s_id, u2.username as usr_s_name, nc.id as idtype, nc.description as noti_descipcion from notification n left join usuarios u on (n.id_usr_recieve = u.id) left join usuarios u2 on (n.id_usr_send = u2.id) left join notifications_codes nc on (n.type_notification = nc.id) where n.id_usr_recieve = $1 and read = false order by n.created_at desc', [id]);
+        const respuesta = await pool.query('select n.id as id, n.type_notification , n.created_at, n.description , n.id_objeto , u.id as usr_r_id, u.username as usr_r_name,u2.id as usr_s_id, u2.username as usr_s_name, nc.id as idtype, nc.description as noti_descipcion from notification n left join usuarios u on (n.id_usr_recibe = u.id) left join usuarios u2 on (n.id_usr_send = u2.id) left join notifications_codes nc on (n.type_notification = nc.id) where n.id_usr_recibe = $1 and read = false order by n.created_at desc', [id]);
         console.log(respuesta.rows);
         res.status(200).json(respuesta.rows);
     }
@@ -21,7 +21,7 @@ exports.readByIdUsr = async function (req, res) {
 exports.countUnreadByUsr = async function (req, res) {
     const id = parseInt(req.params.id)
     try {
-        const respuesta = await pool.query('select count(id) from notification n where n.id_usr_recieve  = $1 and n.read = false', [id]);
+        const respuesta = await pool.query('select count(id) from notification n where n.id_usr_recibe  = $1 and n.read = false', [id]);
         console.log(respuesta);
         res.status(200).json(respuesta.rows[0].count);
     }
